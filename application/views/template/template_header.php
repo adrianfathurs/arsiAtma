083928849fdfb3f1be07f1bca11e5c652640f981
setiawanwcksn <60248993+setiawanwcksn@users.noreply.github.com>
2020-10-14 00:48:45 +0700

    vlogin aman

--- application/views/template/template_header.php
+++ application/views/template/template_header.php
@@ -17,6 +17,7 @@
         crossorigin="anonymous" />
     <!-- main css -->
     <link rel="stylesheet" href="<?php echo base_url('assets/')?>css/style.css" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/')?>css/login.css" />
 </head>
 
 <body>
@@ -159,7 +160,8 @@
                   <a class="nav-link" href="contact.html">PORTOFOLIO</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="contact.html">LOGIN</a>
+                  <a class="nav-link" data-toggle="modal" data-target="#myModal" href="#">LOGIN</a>
+                  
                 </li>
                 
               </ul>
@@ -168,4 +170,65 @@
         </nav>
       </div>
     </header>
+
+
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog modal-sm" role="document">
+                
+                  <div class="modal-header">
+                    
+                  </div>
+                  <div class="modal-content ">
+                  <div class="modal-body">
+                    
+  <div class="">
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+						<img src="<?php echo base_url('assets/')?>img/logo.jpg" class="brand_logo" alt="Logo">
+					</div>
+				</div>
+				<div class="d-flex justify-content-center form_container">
+					<form>
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text" name="" class="form-control input_user" value="" placeholder="username">
+						</div>
+						<div class="input-group mb-2">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" name="" class="form-control input_pass" value="" placeholder="password">
+						</div>
+						<div class="form-group">
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customControlInline">
+								<label class="custom-control-label" for="customControlInline">Remember me</label>
+							</div>
+						</div>
+							<div class="d-flex justify-content-center mt-3 login_container">
+				 	<button type="button" name="button" class="btn login_btn">Login</button>
+				   </div>
+					</form>
+				</div>
+		
+				<div class="mt-4">
+					<div class="d-flex justify-content-center links">
+						Don't have an account? <a href="#" class="ml-2">Sign Up</a>
+					</div>
+					<div class="d-flex justify-content-center links">
+						<a href="#">Forgot your password?</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+                     </form>
+                </div>
+              </div>
+            </div>
+            </div>
 
