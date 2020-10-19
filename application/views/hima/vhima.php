de1f105567c4e09e51cdc6b2e12ab8482ee29e34
setiawanwcksn <60248993+setiawanwcksn@users.noreply.github.com>
2020-10-19 12:36:43 +0700

    tentang hima done

--- application/views/hima/vhima.php
+++ application/views/hima/vhima.php
@@ -12,9 +12,7 @@ if (isset($css)) {
             <div class="row">
                 <div class="col-lg-8 ">
                     <div class="col-lg-12">
-                    <div>
-                    <a href="<?php echo base_url('article/update/')?>" class="btn waves-effect waves-light btn-warning">Edit</a>                                             
-                    </div>
+                    
                         <div class="feature-img">
                             <img class="img-fluid" src="<?php echo base_url('assets/')?>img/blog/feature-img1.jpg" alt="">
                         </div>
@@ -24,27 +22,93 @@ if (isset($css)) {
                             
                             <hr>
                             <br>
-                                <center><h3 class="text-black"><b><span>-- </span> TRICAKA <span> --</span></b></h3></center>
+                                <center><h2 class="text-black"><b><span>-- </span>  Triçaka <span> --</span></b></h2></center>
                             <br>
-                            <p class="text-justify text-black text-arvo">&nbsp;&nbsp;Crast FM merupakan sebuah radio komunitas milik Ilmu Komunikasi Universitas
-                                Pembangunan Nasional “Veteran” Yogyakarta. Radio ini berdiri pada 14 Mei 2003 di
-                                Babarsari, Sleman, Yogyakarta (Kampus II UPN “Veteran” Yogyakarta). Tagline dari Crast
-                                sendiri adalah Smart People Think Different. Crast FM memiliki pendengar setia yang biasa
-                                dipanggil dengan sebutan Smart Listener.</p>
-                                <p class="text-justify text-black text-arvo">&nbsp;&nbsp;Sebagai radio komunitas dengan mahasiswa dan anak muda sebagai sasaran pendengarnya,
-                                Crast FM menyajikan berbagai informasi mengenai dunia musik, hiburan, gaya hidup, dan
-                                informasi menarik lainnya. Selain itu, Crast FM juga menempatkan diri sebagai media
-                                kampus Universitas Pembangunan Nasional “Veteran” Yogyakarta, dengan memberitakan
-                                informasi seputar kampus pada program Speaker dan mengundang narasumber dari kampus
-                                pada program Breakdown. Crast FM mengudara dari Senin sampai Jum’at, mulai pukul
-                                08.00-15.00. Berbagai program siaran yang dimiliki oleh Crast FM diantaranya, Morning
-                                Sunshine, Crast Hour, Speaker Traffic Zone, dan Breakdown.</p>
-                                <p class="text-justify text-black text-arvo">&nbsp;&nbsp;Kini, Crast FM tidak hanya mengudara melalui kanal terestrial di 107.8 Fm. Crast FM sudah
-                                merambah secara streaming melalui platform milik Jogjastreamers.com. Melalui layanan
-                                streaming jangkauan siaran Crast FM tidak terbatas pada wilayah Daerah Istimewa
-                                Yogyakarta. Kini, Smart Listeners dapat mendengarkan Crast FM hingga berbagai penjuru
-                                dunia. Tidak hanya itu, Crast FM juga menyapa Smart Listeners melalui melalui media
-                                sosialnya yaitu Instagram, Youtube, dan Twitter.</p>
+                            <h3>Latar Belakang</h3>
+                            <p class="text-justify text-black text-arvo">&nbsp;&nbsp;
+                                Merupakan organisasi di tingkat Program Studi pada Universitas Atma Jaya Yogyakarta dengan wadah berupa :
+                                Himpunan Mahasiswa Arsitektur Triçaka disebut HIMA Triçaka. <br>
+                                1. Himpunan mahasiswa program studi Arsitektur adalah wahana dan pengembangan diri mahasiswa ke arah                 
+                                akademis yaitu kearsitekturan dalam suatu wawasan dan peningkatan berpikir integritas kepribadian. <br>
+                                2. Kegiatan kemahasiswaan meliputi penalaran secara khusus dan keilmuan, minat dan kegemaran serta upaya
+                                peningkatan kesejahteraan mahasiswa di perguruan tinggi. <br>
+                                3. Dalam peningkatan dan pengembangan bidang akademis, tidak lepas dari tukar pikiran antara akademis
+                                se-program studi pada suatu instansi. <br>
+                                </p>
+                                <h3>Tujuan</h3>
+                                <p class="text-justify text-black text-arvo">
+                                1. Meningkatkan komunikasi dengan saling memperkuat tukar menukar pandangan intern dan ekstern mahasiswa
+                                Arsitektur. <br>
+                                2. Mencari masukan-masukan dari masyarakat guna menunjang pendidikan Arsitektur Universitas Atma Jaya
+                                Yogyakarta. <br>
+                                3. Menyumbang pemikiran khusus bagi dunia pendidikan Arsitektur Universitas Atma Jaya Yogyakarta. <br>
+                                4. Mendarmabaktikan pendidikan Arsitektur Universitas Atma Jaya Yogyakarta pada masyarakat. <br>
+                                5.Memberikan wadah kepada mahasiswa Arsitektur Universitas Atma Jaya Yogyakarta, untuk menyalurkan minat dan
+                                kemampuan di bidang Arsitektur dan bidang lainnya. <br>
+                                6. Menumpuk rasa kebersamaan antar mahasiswa seprogram studi.<br>
+                                Untuk mencapai tujuan dan usaha-usaha tersebut, HIMA Triçaka akan berusaha di bidang : <br>
+                                a. Pendidikan <br>
+                                b. Penelitian dan pengembangan <br>
+                                c. Pengabdian Masyarakat <br>
+                                d. Penunjang, meliputi minat dan bakat</p> <br>
+                                <h3>Visi</h3>
+                                <p class="text-justify text-black text-arvo">&nbsp;&nbsp;                                
+                                Himpunan Mahasiswa HIMA Triçaka sebagai wadah bagi seluruh mahasiswa Arsitektur Atma Jaya Yogyakarta
+                                untuk semakin berperan aktif dalam bidang akademis maupun non akademis, menghimpun seluruh aspirasi dan
+                                kritik dari mahasiswa, serta menjalin komunikasi yang baik sehingga terbentuk relasi yang kuat terhadap sesama,
+                                baik ke dalam maupun ke luar.
+                                </p>
+                                <h3>Misi</h3>
+                                <p class="text-justify text-black text-arvo"><br>
+                                1. Menghimpun seluruh mahasiswa Arsitektur Universitas Atma Jaya Yogyakarta. <br>
+                                2. Mempererat hubungan keluarga Arsitektur Universitas Atma Jaya Yogyakarta. <br>
+                                3. Menjadi media penghubung antara HIMA Tricaka dengan Universitas Atma Jaya Yogyakarta. <br>
+                                4. Menjadi media penghubung antara HIMA Tricaka dengan Universitas lain. <br>
+                                5. Menghimpun aspirasi dan kritik dari mahasiswa. <br>
+                                6. Menyediakan wadah bagi mahasiswa untuk berperan aktif dalam bidang akademis, maupun non akademis. <br>
+                                </p>
+                                <h3>Makna Logo</h3>
+                                <div><center>
+                                <img class="img-fluid" src="<?php echo base_url('assets/')?>img/tentanghima.jpg" style=" max-width: 80%; max-height: 250px;"> </center>
+                                </div> <br>
+                                <p class="text-justify text-black text-arvo">&nbsp;&nbsp;
+                                    Bagaimana hingga akhirnya tercetus sebuah nama "HIMA Triçaka" sebagai identitas dari organisasi?
+                                HIMA Triçaka (A dengan underline dan c dengan ejaan Sansekerta) sebagai aksentuasi/ikonik
+                                mahasiswa arsitektur UAJY untuk membedakan identitas Hima dengan mahasiswa jurusan lain.
+                                Nama HIMA Triçaka, kita adopsi dari lambang Hima dengan tiga pilar itu sebagai 3 chaka (dalam
+                                Bahasa Sansekerta berarti tiang/pilar/kolom). Mengingat kampus UAJY bercirikan Katolikisme maka
+                                Lambang Hima dipilih menggunakan 3 pilar itu sebagai Tritunggal Maha Kudus (+), yang memiliki
+                                lambang Trinitas dan Tri Dharma dengan makna kemuliaan dan Roh Kudus dalam perjalanan
+                                Himpunan. <br> <br>
+                                Filosofi Triçaka : Pendidikan, penulisan dan penelitian, dan pengabdian pada masyarakat. <br>
+                                Semboyan : Kekeluargaan, loyalitas, dan tanggung jawab. <br>
+                                Penulisan : HIMA Triçaka (A dengan underline dan c dengan ejaan Sansekerta). <br> <br>
+                                <h6> SEPASANG TIANG PENYANGGA </h6>
+                                Sebagai penyangga/penguat di bidang kegiatan HIMA Triçaka sekaligus simbol tempat atau wadah penyalur inspirasi dan
+                                kreasi atas karya mahasiswa arsitektur <br><br>
+                                <h6>ATAP DAN LAMBANG MERPATI </h6>
+                                UAJY sebagai pengayom dan pelindung segala bentuk kegiatan Mahasiswa Arsitektur UAJY dalam wadahnya, yaitu HIMA
+                                Triçaka. <br><br>
+                                <h6>TANGGA ATAU UNDAKAN </h6>
+                                Sebagai usaha yang dilakukan/telah dilakukan dan diupayakan dengan semaksimal mungkin dari tercapainya cita-cita dan
+                                tujuan HIMA Triçaka. <br><br>
+                                <h6>UNTAIAN KAIN PADA ORANG </h6>
+                                Semboyan yang ada pada HIMA Triçaka selalu mengilhami setiap ide, tindakan dan cara berpikir dari Mahasiswa Arsitektur
+                                UAJY khususnya dalam hal ini setiap langkah dan kegiatan HIMA Triçaka.
+                                </p>
+                                <p class="text-justify text-black text-arvo">
+                                &nbsp;&nbsp; <i> Apa proker pertama yang dijalankan di HIMA Triçaka? </i><br>
+                                “Proker kami saat itu adalah mengadakan lomba untuk merancang lambang/logo Hima, pemenangnya lambang
+                                Hima yang seperti sekarang itu Triçaka. Pemenangnya adalah Vebriano (angkatan 1989), yang juga mendesain
+                                logo UAJY yang saat itu akan ganti logo. Mengenai lambang terpilih itu lebih tepat bila dinamakan sebagai lambang
+                                (bukan logo) HIMA, kalau lambang itu sama halnya simbol yang mempunyai sifat permanen sedangkan logo sifat
+                                desainnya atraktif, dekoratif yang bersifat temporer untuk event khusus.” <br>
+                                &nbsp;&nbsp; <i> Bagaimana kisah suka duka kepengurusan HIMA Triçaka yang pertama? </i> <br>
+                                “Sukanya, ya kita masih menikmati belajar berorganisasi baru yang pertama di himpunan mahasiswa arsitektur
+                                UAJY dan kampus support penuh namun dukanya adalah Hima ini organisasi baru, tentunya mahasiswa belum
+                                begitu kenal dengan HIMA dan SDM dalam organisasi terbatas.”
+
+                                </p>
                         </div>
             </div>
             
