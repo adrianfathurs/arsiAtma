4e6cd43d3314490d4876749ce5d1a299daf55716
setiawanwcksn <60248993+setiawanwcksn@users.noreply.github.com>
2021-04-03 20:52:58 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.dummyarsiatma.epizy.com/';
-//  $config['base_url'] = 'http://localhost/arsiAtma/';
+// $config['base_url'] = 'http://www.dummyarsiatma.epizy.com/';
+ $config['base_url'] = 'http://localhost/arsiAtma/';
 
 
 /*
